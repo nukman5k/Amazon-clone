a7b0d757127b06280e4aa598210ccb9c4bd5dc30
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-12-01 22:54:47 +0200

    update

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -5,7 +5,7 @@ const ProductFeed = ({ products }) => {
     return (
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
              
-            {products.map(({ id, title, price, description, category, image }) => (
+            {products.slice(0,4).map(({ id, title, price, description, category, image }) => (
                 <Product
                     key={id}
                     id={id}
@@ -17,7 +17,7 @@ const ProductFeed = ({ products }) => {
                 />
             ))}
 
-            <img className='md:col-span-4' src="https://links.papareact.com/dyz" alt="" />
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
         </div>
     );
 }
