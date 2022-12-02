3bd25ec6472143f1b56e7be8730f18c50f2c646f
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-12-02 16:17:43 +0200

    update

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,8 +4,35 @@ import Product from './Product';
 const ProductFeed = ({ products }) => {
     return (
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-             
-            {products.slice(0,4).map(({ id, title, price, description, category, image }) => (
+
+            {products.slice(0, 4).map(({ id, title, price, description, category, image }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            ))}
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
+
+            <div className='md:col-span-2'>
+                {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                ))}
+            </div>
+
+            {products.slice(5, products.length).map(({ id, title, price, description, category, image }) => (
                 <Product
                     key={id}
                     id={id}
@@ -17,7 +44,6 @@ const ProductFeed = ({ products }) => {
                 />
             ))}
 
-            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
         </div>
     );
 }
