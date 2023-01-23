19f2955d973a24820fc5898da97b20d417efeee5
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2023-01-23 10:51:53 +0200

    UPDATE FINAL

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    images: {
-        domains: ["links.papareact.com", "fakestoreapi.com"]
-    },
+  images: {
+    domains: ["links.papareact.com", "fakestoreapi.com"],
+  },
+  stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+//   stripe_secret_key: process.env.STRIPE_SECRET_KEY,
 };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,14 +6,37 @@ import { useSelector } from 'react-redux';
 import { selectItems, selectTotal } from '../slices/basketSlice';
 import Currency from 'react-currency-formatter';
 import { useSession } from 'next-auth/react';
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+
+// const stripePromise = loadStripe(`${process.env.stripe_public_key}`);
+const stripePromise = loadStripe('pk_test_9E0cQDZGT6ceCkAwdVmMNKqx');
 
 const Checkout = () => {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const { data: session } = useSession();
 
-    console.log(total);
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
 
+        //Call the backend to create a checkout session
+        const checkoutSession = await axios.post('/api/create-checkout-session',
+            {
+                items: items,
+                email: session.user.email,
+            });
+
+        //Redirect user to Stripe Checkout
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id
+        })
+
+        if (result.error) {
+            alert(result.error.message);
+        }
+
+    };
 
     return (
         <div className='bg-gray-100'>
@@ -57,11 +80,13 @@ const Checkout = () => {
                             <h2 className='whitespace-nowrap'>
                                 Subtotal ({items.length} items):{" "}
                                 <span className='font-bold'>
-                                    <Currency quantity={total} currency='USD'/>
+                                    <Currency quantity={total} currency='USD' />
                                 </span>
                             </h2>
 
                             <button
+                                role='link'
+                                onClick={createCheckoutSession}
                                 disabled={!session}
                                 className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
                                     }`}>
